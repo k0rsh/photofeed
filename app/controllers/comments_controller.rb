e5e4d047b5a81a5f04a7ed8a293a5a82e8e63d83
k0rsh <korsh@yandex.ru>
2017-02-13 16:43:54 +0300

    Added comment feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "Your comment has been added!"
+      redirect_to :back
+    else
+      flash[:alert] = "Opps! Something went completly wrong!"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
+    flash[:success] = "Comment deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
