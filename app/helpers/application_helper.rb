76206c854b17f84d0f80bf5f1d3d3686f73f1c89
k0rsh <korsh@yandex.ru>
2017-02-14 17:00:33 +0300

    Added image preview in new & edit post forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,14 @@ module ApplicationHelper
     { success: 'alert-success',
       error: 'alert-danger',
       alert: 'alert-warning',
-      notice: 'alert-info'
-    }[flash_type.to_sym] || flash_type.to_s
+      notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def generate_image_tag_for_post(post)
+    if post.image.exists?
+      image_tag post.image.url(:medium), id: 'image-preview', class: 'img-responsive'
+    else
+      image_tag 'placeholder.png', id: 'image-preview', class: 'img-responsive'
+    end
   end
 end
